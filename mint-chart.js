27d78ba6b2fee5859a87a974d38fd229db839f77
Libo Liu <liboliu@usc.edu>
2018-12-26 14:52:23 -0800

    Highcharts.theme

--- mint-chart.js
+++ mint-chart.js
@@ -7,7 +7,7 @@ import 'highcharts/modules/exporting';
 import 'highcharts/modules/drilldown';
 import 'highcharts/modules/series-label';
 import 'highcharts/modules/export-data';
-import 'highcharts/es-modules/themes/gray';
+import 'highcharts/themes/grays';
 // console.logHighchartsMore;
 // loadExporting(Highcharts);
 // loadDrilldown(Highcharts);
@@ -21,7 +21,7 @@ import 'highcharts/es-modules/themes/gray';
  * @demo demo/index.html
  */
 
-const HighchartsColors = Highcharts.theme.color;
+const HighchartsColors = Highcharts.themes.color;
 // ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
             // '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'];
 class MintChart extends PolymerElement {
